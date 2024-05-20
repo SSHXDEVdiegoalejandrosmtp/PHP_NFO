94fd0f92e89fa4169546bc7d6a16b17214468e56
xzyAlejandroSMTP <sshxdevdiegoalejandrogo@gmail.com>
2024-05-20 02:17:55 +0100

    info php>

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php 
+phpinfo();
+phpinfo(int $flags = INFO_ALL):true
+phpinfo(INFO_MODULES);
+phpinfo(INFO_CREDITS);
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Diegoalejandrobackendphpdevphp</title>
+</head>
+<body>
+<?php echo "  HI  diegoalejandro   "      ; 
+array = phpinfo_array();
+$array["sys" ]["sytem"];
+
+exit()
+
+
+
+?>   
+
+</body>
+</html>
+
